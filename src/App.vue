6fa555c8b85e1c40843285671c4d4844532f576f
Niniatura <neli.marcos@gmail.com>
2022-03-15 08:46:12 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <formulario-web></formulario-web>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormularioWeb from './components/FormularioWeb.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormularioWeb
   }
 }
 </script>
---
+++ src/components/FormularioWeb.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+      <form action="" @submit.prevent="contacto">
+          <label for="nombre">Nombre</label>
+          <input 
+            type="text"
+            v-model="name"
+            id="nombre"
+            required
+            placeholder="Ingresar nombre">
+
+            <label for="email">Email</label>
+            <input 
+            type="email"
+            v-model="email"
+            id="email"
+            required
+            placeholder="Ingresar email">
+
+            <label for="password">Password</label>
+            <input 
+            type="password"
+            v-model="password"
+            id="password"
+            required
+            placeholder="Ingresar password">
+
+            <p v-if="editedPassword && !passwordHasNumbers">Error. La contraseña debe tener al menos un número</p>
+
+            <input type="submit" value="Contactar">
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'FormularioWeb',
+    data(){
+        return{
+            name:'',
+            email:'',
+            editedPassword: null
+        }
+    },
+    computed: {
+        passwordHasNumbers(){
+            return /\d/.test(this.editedPassword);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
